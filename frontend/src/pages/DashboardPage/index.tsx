375bc3f8b42de5510d7ffa9fc5232cb39077dbda
Dmitriy <vahrameev.work@gmail.com>
2024-05-12 03:00:50 +0300

    update add finance statements

--- frontend/src/pages/DashboardPage/index.tsx
+++ frontend/src/pages/DashboardPage/index.tsx
@@ -2,10 +2,11 @@ export const DashboardPage = () => {
     return (
         <>
             <iframe
-                title="кредиты"
+                title="финансовые_показатели"
                 width="1140"
-                height="800"
-                src="https://app.powerbi.com/reportEmbed?reportId=5a4d208a-fca1-4aa1-9984-2a46a2e5434b&autoAuth=true&ctid=24c0c468-d699-47a3-b18f-d304d154105d"
+                height="541.25"
+                src="https://app.powerbi.com/reportEmbed?reportId=ec62fb83-c33f-44e9-819e-cec03bea0297&autoAuth=true&ctid=24c0c468-d699-47a3-b18f-d304d154105d"
+                frameBorder="0"
                 allowFullScreen={true}
             ></iframe>
         </>
