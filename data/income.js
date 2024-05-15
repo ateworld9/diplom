ef40019f97295c68feff0c2c79864225f8df9935
Dmitriy <vahrameev.work@gmail.com>
2024-05-15 11:05:19 +0300

    update data

--- data/income.js
+++ data/income.js
@@ -13,25 +13,24 @@ async function write(file, content) {
     }
 }
 
-dayjs("2017-01-01").unix();
-dayjs().unix();
+// dayjs("2017-01-01").unix();
+// dayjs().unix();
 
 const request = async () => {
     const options = {
         method: "GET",
-        url: "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-timeseries",
+        url: "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/modules",
         params: {
-            symbol: "AAPL",
-            period1: dayjs("2017-01-01").unix(),
-            period2: dayjs().unix(),
-            region: "US",
+            ticker: "AAPL",
+            module: "income-statement",
         },
         headers: {
             "X-RapidAPI-Key":
                 "a1090c2a4dmsh3e5c825152c387cp139f3fjsn45a1e13e1e19",
-            "X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+            "X-RapidAPI-Host": "yahoo-finance15.p.rapidapi.com",
         },
     };
+
     try {
         const response = await axios.request(options);
         write("./income.json", JSON.stringify(response.data));
--- frontend/src/pages/DashboardPage/index.tsx
+++ frontend/src/pages/DashboardPage/index.tsx
@@ -4,7 +4,7 @@ export const DashboardPage = () => {
             <iframe
                 title="финансовые_показатели"
                 width="1140"
-                height="541.25"
+                height="800"
                 src="https://app.powerbi.com/reportEmbed?reportId=ec62fb83-c33f-44e9-819e-cec03bea0297&autoAuth=true&ctid=24c0c468-d699-47a3-b18f-d304d154105d"
                 frameBorder="0"
                 allowFullScreen={true}
