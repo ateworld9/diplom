87d16c4bd2c8f2dfab03cca1902005ccd432d763
Dmitriy <vahrameev.work@gmail.com>
2024-05-15 17:12:30 +0300

    add accordions

--- frontend/src/components/mainListItems.tsx
+++ frontend/src/components/mainListItems.tsx
@@ -8,48 +8,66 @@ import Divider from '@mui/material/Divider';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import TableRowsOutlinedIcon from '@mui/icons-material/TableRowsOutlined';
 import TrendingUpOutlinedIcon from '@mui/icons-material/TrendingUpOutlined';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
 
 export const mainListItems = (
     <>
-        <ListSubheader component="div">Apple</ListSubheader>
-        <ListItemButton LinkComponent={Link} to="/apple-dashboard">
-            <ListItemIcon>
-                <DashboardIcon />
-            </ListItemIcon>
-            <ListItemText primary="Финансовые показатели" />
-        </ListItemButton>
-        <ListItemButton LinkComponent={Link} to="/apple-balance-sheet">
-            <ListItemIcon>
-                <TableRowsOutlinedIcon />
-            </ListItemIcon>
-            <ListItemText primary="Сводная таблица" />
-        </ListItemButton>
-        <ListItemButton LinkComponent={Link} to="/apple-cashflow">
-            <ListItemIcon>
-                <TrendingUpOutlinedIcon />
-            </ListItemIcon>
-            <ListItemText primary="Финансовый поток" />
-        </ListItemButton>
+        <Accordion disableGutters square>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <ListSubheader component="div">Apple</ListSubheader>
+            </AccordionSummary>
+            <AccordionDetails>
+                <ListItemButton LinkComponent={Link} to="/apple-dashboard">
+                    <ListItemIcon>
+                        <DashboardIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Финансовые показатели" />
+                </ListItemButton>
+                <ListItemButton LinkComponent={Link} to="/apple-balance-sheet">
+                    <ListItemIcon>
+                        <TableRowsOutlinedIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Сводная таблица" />
+                </ListItemButton>
+                <ListItemButton LinkComponent={Link} to="/apple-cashflow">
+                    <ListItemIcon>
+                        <TrendingUpOutlinedIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Финансовый поток" />
+                </ListItemButton>
+            </AccordionDetails>
+        </Accordion>
         <Divider />
-        <ListSubheader component="div">Alibaba</ListSubheader>
-        <ListItemButton LinkComponent={Link} to="/alibaba-dashboard">
-            <ListItemIcon>
-                <DashboardIcon />
-            </ListItemIcon>
-            <ListItemText primary="Финансовые показатели" />
-        </ListItemButton>
-        <ListItemButton LinkComponent={Link} to="/alibaba-balance-sheet">
-            <ListItemIcon>
-                <TableRowsOutlinedIcon />
-            </ListItemIcon>
-            <ListItemText primary="Сводная таблица" />
-        </ListItemButton>
-        <ListItemButton LinkComponent={Link} to="/alibaba-cashflow">
-            <ListItemIcon>
-                <TrendingUpOutlinedIcon />
-            </ListItemIcon>
-            <ListItemText primary="Финансовый поток" />
-        </ListItemButton>
+
+        <Accordion disableGutters square>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <ListSubheader component="div">Alibaba</ListSubheader>
+            </AccordionSummary>
+            <AccordionDetails>
+                <ListItemButton LinkComponent={Link} to="/alibaba-dashboard">
+                    <ListItemIcon>
+                        <DashboardIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Финансовые показатели" />
+                </ListItemButton>
+                <ListItemButton
+                    LinkComponent={Link}
+                    to="/alibaba-balance-sheet"
+                >
+                    <ListItemIcon>
+                        <TableRowsOutlinedIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Сводная таблица" />
+                </ListItemButton>
+                <ListItemButton LinkComponent={Link} to="/alibaba-cashflow">
+                    <ListItemIcon>
+                        <TrendingUpOutlinedIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Финансовый поток" />
+                </ListItemButton>
+            </AccordionDetails>
+        </Accordion>
         <Divider />
     </>
 );
